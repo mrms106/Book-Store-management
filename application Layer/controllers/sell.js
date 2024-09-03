f408990d66584c31e95096dfc0d1333a53710a2c
Makardhwaj <mhs93679@gmail.com>
2024-09-03 15:44:29 +0530

    fix the sells data showing multple time

--- application Layer/controllers/sell.js
+++ application Layer/controllers/sell.js
@@ -42,39 +42,42 @@ module.exports.sellsmulti = async (req, res) => {
     const sellData = req.body;
 
     if (Array.isArray(sellData.books) && sellData.books.length > 0) {
-        // Multiple book sale
+        const bulkOperations = sellData.books.map(async (book) => {
+            const bookId = book.bookId;
+            const sellStock = book.stock;
+        
+            const bookDoc = await books.findById(bookId);
+            if (!bookDoc) {
+                throw new Error(`Book with ID ${bookId} not found`);
+            }
+        
+            const bookStock = bookDoc.stock - sellStock;
+            if (bookStock < 0) {
+                throw new Error(`Insufficient stock for book ID ${bookId}`);
+            }
+        
+            await books.findByIdAndUpdate(bookId, { stock: bookStock, inBasket: false }, { new: true });
+        
+            // Add this book's data to the sellRecord
+            return {
+                price: book.price,
+                stock: book.stock,
+                bookname: book.title
+            };
+        });
+        
         try {
-            const bulkOperations = sellData.books.map(async (book) => {
-                const bookId = book.bookId;
-                const sellStock = book.stock;
-
-                const bookDoc = await books.findById(bookId);
-                if (!bookDoc) {
-                    throw new Error(`Book with ID ${bookId} not found`);
-                }
-
-                const bookStock = bookDoc.stock - sellStock;
-                if (bookStock < 0) {
-                    throw new Error(`Insufficient stock for book ID ${bookId}`);
-                }
-
-                await books.findByIdAndUpdate(bookId, { stock: bookStock,inBasket:false }, { new: true });
-
-                const sellRecord = {
-                    name: sellData.name,
-                    phone: sellData.phone,
-                    date: sellData.date,
-                    bookdata:sellData.books.map(book=>({
-                        price: book.price,
-                        stock: book.stock,
-                        bookname: book.title
-                    }))
-                };
-
-                await sell.create(sellRecord);
-            });
-
-            await Promise.all(bulkOperations);
+            const bookDataArray = await Promise.all(bulkOperations);
+        
+            const sellRecord = {
+                name: sellData.name,
+                phone: sellData.phone,
+                date: sellData.date,
+                bookdata: bookDataArray
+            };
+        
+            await sell.create(sellRecord);
+        
             res.status(200).json({ message: "All sales were successful" });
         } catch (err) {
             console.log(err);
